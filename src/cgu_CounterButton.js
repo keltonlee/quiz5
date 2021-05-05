8779668b30f87c5e4c123ae475be1a6e59ec1fbf
keltonlee <keltonyt@gmail.com>
2021-05-05 22:26:25 +0800

    修改

--- src/cgu_CounterButton.js
+++ src/cgu_CounterButton.js
@@ -8,13 +8,11 @@ export default class CounterButton extends React.Component {
     this.handleClick = this.handleClick.bind(this)
     }
     handleClick(event) {
-    this.setState({ i: this.props.i});
     this.setState({ counter: this.state.counter+1});
     console. log(this.state);
     }
     render() {
     var text = this.state.counter;
-    var i = this.state.i;
-    return(<button onClick={this.handleClick}>第{i}個按鍵 你點了 {text}次</button>);
+    return(<button onClick={this.handleClick}>第{this.props.i}個按鍵 你點了 {text}次</button>);
     }
     }
